89713b9f9c2894c8858c5ffa14a345d1fc604786
Alexander Krupenkin <mail@akru.me>
2019-01-09 14:39:51 +0300

    Init repo

---
+++ contracts/Collateral.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol';
+
+contract Collateral is ERC20Mintable, ERC20Burnable, ERC20Detailed {
+    constructor(uint256 _initial_supply) public ERC20Detailed("Collateral token", "CLT", 18) {
+        _mint(msg.sender, _initial_supply);
+    }
+}
---
+++ contracts/IDEX.sol
@@ -0,0 +1,88 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+contract IDEX {
+    /**
+     * @dev Event emitted for every registered order in a contract.
+     */
+    event TradeOpened(uint256 indexed trade_id);
+
+    /**
+     * @dev Event emitted for every closed trade in a contract.
+     */
+    event TradeClosed(uint256 indexed trade_id);
+
+    /**
+     * @dev Open trade by matching two orders.
+     * @param _maker_data off-chain offer params(external account, nonce, etc.)
+     * @param _maker_value value in maker token
+     * @param _maker_deadline maker message deadline block
+     * @param _maker_signature EC signature of maker message*
+     * @param _taker_data off-chain demand params (external account, nonce, etc.)
+     * @param _taker_value value in taker token
+     * @param _taker_deadline taker message deadline block
+     * @param _taker_signature EC signature of taker message*
+     * @param _collateral_value value of collateral token that will be send as refund
+     * @param _oracles list of approved oracle accounts
+     * @param _min_oracles_count count of oracles signatures required for transfer confirmation
+     * @return positive number corresponds to trade id
+     */
+    function open_trade(
+        bytes calldata _maker_data,
+        uint256 _maker_value,
+        uint256 _maker_deadline,
+        bytes calldata _maker_signature,
+        bytes calldata _taker_data,
+        uint256 _taker_value,
+        uint256 _taker_deadline,
+        bytes calldata _taker_signature,
+        uint256 _collateral_value,
+        address[] calldata _oracles,
+        uint256 _min_oracles_count
+    ) external returns (
+        uint256 trade_id
+    );
+
+    /**
+     * @dev Close trade and pay refund when transfer isn't confirmed.
+     * @param _trade_id trade identifier
+     * @return true when success closed
+     */
+    function close_trade(
+        uint256 _trade_id
+    ) external returns (
+        bool success
+    );
+
+    /**
+     * @dev Get trade by id
+     * @param _trade_id trade identifier
+     */
+    function trade_of(
+        uint256 _trade_id
+    ) external view returns (
+        bool
+    );
+
+    /**
+     * @dev Cancel order before trading.
+     */
+    function cancel_order(
+        bytes calldata _data,
+        uint256 _value,
+        uint256 _deadline,
+        bytes calldata _signature,
+        uint256 _collateral_value,
+        address[] calldata _oracles,
+        uint256 _min_oracles_count
+    ) external returns (bool);
+
+    /**
+     * @dev Confirm transfer by hash.
+     * @param _data_hash transaction descriptor hash
+     * @notice oracles call only
+     */
+    function confirm_transfer(
+        bytes32 _data_hash
+    ) external returns (bool);
+    
+}
